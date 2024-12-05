5705b53720d92ceff1fad834bd3c641a9101d6fa
Arjun Pokharel <arjunpokharel10@gmail.com>
2024-12-05 17:29:17 +1100

    test to commit the changes to some codes

--- challenge_set_1/revise_numbers_and_floats.py
+++ challenge_set_1/revise_numbers_and_floats.py
@@ -4,3 +4,54 @@
 # ● Their difference
 # ● Their product
 # ● Their division (handle the case where division by zero might occur).
+
+
+from decimal import DivisionByZero
+
+
+def take_numbers_for_calculation():
+    num_digits = []
+    for i in range(2):
+        while True:
+            try:
+                x = int(input(f"enter {'your first' if i == 0 else 'your second'} number: "))
+                num_digits.append(x)
+                break
+            except ValueError:
+                print("enter a valid numeric digit and try again!")
+    return num_digits
+
+def calc_add():
+    nums = take_numbers_for_calculation()
+    result_sums = nums[0] + nums[1]
+    return result_sums
+
+def calc_sub():
+    nums = take_numbers_for_calculation()
+    result_diff = nums[0] - nums[1]
+    return result_diff
+    
+
+def calc_prd():
+    nums = take_numbers_for_calculation()
+    result_mult = nums[0] * nums[1]
+    return result_mult
+
+def calc_div():
+    while True:
+        try:
+            nums = take_numbers_for_calculation()
+            result_divi = nums[0] / nums[1]
+            break
+        except ZeroDivisionError:
+            print("cannot divide by zero!")
+    return result_divi
+
+
+result = calc_div()
+print(result)
+
+
+
+
+
