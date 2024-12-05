b366719b4b36fdea85ac8d83a2e70807859ccb24
Arjun Pokharel <arjunpokharel10@gmail.com>
2024-12-05 19:25:52 +1100

    to update the file name: revise_lists.py

--- challenge_set_1/revise_lists.py
+++ challenge_set_1/revise_lists.py
@@ -3,3 +3,11 @@
 # ● Returns the largest and smallest number in the list.
 # ● Removes duplicates from the list.
 # ● Sorts the list in descending order.
+
+def remove_duplicates_and_sort_given_numbers():
+    x = input("enter some numeric values in digits, separated by space: ")
+    x = list(set(map(int, x.replace(',',' ').split())))
+    x = sorted(x)
+    print(f"the input in sorted order, and without duplicates, is: {x}\nthe smallest is {x[0]} and the largest is {x[-1]}")
+
+remove_duplicates_and_sort_given_numbers()
