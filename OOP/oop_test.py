91d90d75e782c262dc03ca1b2b18d469eb228397
Arjun Pokharel <arjunpokharel10@gmail.com>
2024-12-07 12:04:28 +1100

    update to challenge set 1

---
+++ OOP/oop_test.py
@@ -0,0 +1,8 @@
+# SYNTAX
+
+class ClassName:
+    def __init__(self):
+        pass
+
+    def method(self):
+        pass
--- challenge_set_1/04_revise_tuples.py
+++ challenge_set_1/04_revise_tuples.py
@@ -2,3 +2,4 @@
 # ● Takes a tuple of integers as input.
 # ● Returns a new tuple with only the even numbers from the original tuple.
 # ● Converts the tuple into a list and adds a new element to it.
+
